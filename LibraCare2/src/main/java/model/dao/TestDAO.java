e02bd68bdb0500e32338353f23bb3ceb1cefda17
Administrator <Administrator@PC-number>
2024-05-24 17:04:36 +0900

    初回コミット

---
+++ LibraCare2/src/main/java/model/dao/TestDAO.java
@@ -0,0 +1,21 @@
+package model.dao;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Servlet implementation class TestDAO
+ */
+@WebServlet("/TestDAO")
+public class TestDAO extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TestDAO() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+}
