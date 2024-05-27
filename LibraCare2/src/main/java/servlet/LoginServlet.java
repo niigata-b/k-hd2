83be48f970bea1e9e1bed7f50cd6eabba98a169b
Administrator <Administrator@PC-number>
2024-05-27 11:28:01 +0900

    kaneko

---
+++ LibraCare2/src/main/java/servlet/LoginServlet.java
@@ -0,0 +1,74 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.dao.AdminDAO;
+
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String url = null;
+
+		request.setCharacterEncoding("UTF-8");
+
+		String admin_id = request.getParameter("admin_id");
+		String password = request.getParameter("password");
+
+		try {
+			AdminDAO adminDao = new AdminDAO();
+
+			if (adminDao.loginCheck(admin_id, password)) {
+				url = "menu";
+
+				HttpSession session = request.getSession();
+				session.setAttribute("admin_id", admin_id);
+
+			} else {
+				url = "top-menu-login.jsp";
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		RequestDispatcher rd = request.getRequestDispatcher(url);
+		rd.forward(request, response);
+
+	}
+
+}
---
+++ LibraCare2/src/main/java/servlet/MenuServlet.java
@@ -0,0 +1,61 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class MenuServlet
+ */
+@WebServlet("/menu")
+public class MenuServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public MenuServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		String url = null;
+
+		// セッションオブジェクトの取得
+		HttpSession session = request.getSession();
+
+		// ログイン認証済みかどうかを確認
+		if (session.getAttribute("admin_id") != null) {
+			// 認証済み
+			url = "top-menu.jsp";
+		} else {
+			// 未認証
+			url = "top-menu-login.jsp";
+		}
+
+		RequestDispatcher rd = request.getRequestDispatcher(url);
+		rd.forward(request, response);
+
+	}
+}
