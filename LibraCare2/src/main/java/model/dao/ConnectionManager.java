574e569b5fcc621c06325034392cdf6788465a41
Administrator <Administrator@PC-number>
2024-05-27 09:44:22 +0900

    コネ真似河野

--- LibraCare2/src/main/java/model/dao/ConnectionManager.java
+++ LibraCare2/src/main/java/model/dao/ConnectionManager.java
@@ -1,21 +1,42 @@
 package model.dao;
 
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
- * Servlet implementation class ConnectionManager
+ * コネクションマネージャ
+ * @author emBex Education
  */
-@WebServlet("/ConnectionManager")
-public class ConnectionManager extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ConnectionManager() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+public class ConnectionManager {
 
+	/**
+	 * データベースURL
+	 */
+	private final static String URL = "jdbc:mysql://localhost:3306/library_db";
+
+	/**
+	 * ユーザ
+	 */
+	private final static String USER = "admin";
+
+	/**
+	 * パスワード
+	 */
+	private final static String PASSWORD = "password";
+
+	/**
+	 * データベースへの接続を取得して返します。
+	 *
+	 * @return コネクション
+	 * @throws SQLException
+	 * @throws ClassNotFoundException
+	 */
+	public static Connection getConnection() throws SQLException, ClassNotFoundException {
+
+		// JDBCドライバの読み込み
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		return DriverManager.getConnection(URL, USER, PASSWORD);
+
+	}
 }
