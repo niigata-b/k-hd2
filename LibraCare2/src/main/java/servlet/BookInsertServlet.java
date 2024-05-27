d2c31712bea63604a14794ca146a75ed2d7c5c45
Administrator <Administrator@PC-number>
2024-05-27 15:38:07 +0900

    a

--- LibraCare2/src/main/java/servlet/BookInsertServlet.java
+++ LibraCare2/src/main/java/servlet/BookInsertServlet.java
@@ -1,11 +1,17 @@
 package servlet;
 
 import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import model.dao.BookDAO;
+import model.entity.BookBean;
 
 /**
  * Servlet implementation class BookInsertServlet
@@ -35,7 +41,50 @@ public class BookInsertServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		request.setCharacterEncoding("UTF-8");
+
+		String isbn = request.getParameter("isbn");
+		String book_name = request.getParameter("book_name");
+		String category_name = request.getParameter("category_name");
+		int book_count = 1;
+		int total_book_count = 1;
+		
+		String url = null;
+		int count = 0;
+
+		BookBean book = new BookBean();
+		BookDAO dao = new BookDAO();
+
+		book.setIsbn(isbn);
+		book.setBookName(book_name);
+		book.setCategoryName(category_name);
+		book.setBookCount(book_count);
+		book.setTotalBookCount(total_book_count);
+		
+		try {
+			// DAOの利用
+			count = dao.bookinsert(book);
+
+			// リクエストスコープへの属性の設定
+			request.setAttribute("book", book);
+
+			if(count != 0)
+			{
+				url = "book-insert-result.jsp";
+			}
+			else
+			{
+				url = "book-insert-failure.jsp";
+			}
+
+		}catch (SQLException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		// リクエストの転送
+		RequestDispatcher rd = request.getRequestDispatcher(url);
+		rd.forward(request, response);	
 	}
 
 }
