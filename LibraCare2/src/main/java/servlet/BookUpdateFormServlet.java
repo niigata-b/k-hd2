d39402af5af7c4191d0c9580136fc5484258a01e
Administrator <Administrator@PC-number>
2024-05-27 10:05:15 +0900

    あ

--- LibraCare2/src/main/java/servlet/BookUpdateFormServlet.java
+++ LibraCare2/src/main/java/servlet/BookUpdateFormServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class BookUpdateFormServlet
  */
-@WebServlet("/bookupdateformservlet")
+@WebServlet("/bookupdateform")
 public class BookUpdateFormServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
